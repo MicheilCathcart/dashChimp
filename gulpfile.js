e19ac271620327a67ba184c780e0dee3b1553e2f
Micheil Cathcart <Micheil@iMac-4.local>
2016-12-12 18:02:03 +1000

    Continuing Upgrade

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,22 @@
 var gulp = require('gulp'),
-nodemon = require('gulp-nodemon');
+	browserify = require("browserify"),
+	tslint = require("gulp-tslint"),
+	tsc = require("gulp-typescript"),
+	sourcemaps = require("gulp-sourcemaps"),
+	runSequence = require("run-sequence"),
+	browserSync = require('browser-sync').create(),
+	nodemon = require('gulp-nodemon');
 
-gulp.task('default', function(){
+gulp.task('default', function () {
 	nodemon({
-		script: 'app.js',
-		ext: 'js css html scss',
-		env: {
-			PORT:9000 
-		},
-		ignore:['./node_modules/**']
-	})
-	.on('restart', function(){
-		console.log('Restarting');
-	})
+			script: 'app.js',
+			ext: 'js css html scss',
+			env: {
+				PORT: 9000
+			},
+			ignore: ['./node_modules/**']
+		})
+		.on('restart', function () {
+			console.log('Restarting');
+		})
 })
-
